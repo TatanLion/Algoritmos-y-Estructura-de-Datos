6c3e6878bb8a21873ebcbaad031e930bda86a742
Jonathan <jonathanmmendieta@gmail.com>
2022-10-17 07:25:05 -0500

    Algoritmos funcionales en la estructura de datos

---
+++ arte.py
@@ -0,0 +1,16 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
+
+def draw(myTurtle, length):
+    if length > 0:
+        myTurtle.forward(length)
+        myTurtle.left(90)
+        draw(myTurtle, length-10)
+
+
+draw(myTurtle, 100)
+myWin.exitonclick()
+
+# Se ejecuta aqui: https://repl.it/languages
